3ff119c1c648147bfe44a2350371010b4a16bcc5
Rosalía <r.doporto@udc.es>
2024-03-29 19:57:56 +0100

    formulario

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -1,12 +1,20 @@
 <?php 
-	$nombre = $_POST['name'];
-	$telefono = $_POST['tel'];
-	$email = $_POST['email'];
-	$asunto = 'Formulario Rellenado';
-	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['message'];
+	//Creamos las variables donde almacenamos los datos del formulario y las formateamos correctamente
+	$nombre = strip_tags(htmlspecialchars($_POST['name']));
+	$email = strip_tags(htmlspecialchars($_POST['email']));
+	$telefono = strip_tags(htmlspecialchars($_POST['tel']));
+	$mensaje = strip_tags(htmlspecialchars($_POST['message']));
 
 
-	if(mail('rdoportoregueiro@gmail.com', $asunto, $mensaje)){
-		echo "Correo enviado";
-	}
+	//Utilizamos estas variables para generar el correo electrónico
+	$to = "rdoportoregueiro@gmail.com"; 
+	$subject = "Formulario web:  $name";
+	$body = "Alguien quiere contactar contigo .\n\n"."Aquí tienes los detalles del mensaje:\n\n 
+	Nombre: $nombre \n\n
+	Email: $email \n\n
+	Telefono: $telefono \n\n
+	Mensaje: $mensaje";
+ 
+	//invocamos el método mail() con sus correspondientes parámetros para enviar el correo según los datos recibidos
+	mail($to, $subject, $body)
  ?>
